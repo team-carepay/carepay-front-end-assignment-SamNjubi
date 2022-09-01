506db292c7cb054f991ae4a3061b313bee3a3bba
samuel macharia <samuelmacharia648@gmail.com>
2022-09-01 15:38:21 +0300

    update initial UI/UX, update requirements logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TreatmentsComponent } from './treatments/treatments.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TreatmentsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
